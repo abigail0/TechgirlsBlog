b3187d62c7801b3059fe80b0a6b71924078e1895
KatieFeatherstone <k.featherstone@gmail.com>
2017-04-04 17:46:24 +0100

    login page WIP

--- insertValues.sql
+++
@@ -1,5 +0,0 @@
-insert hobbies (Firstname, Lastname, Hobby)
-values ('Katie', 'Feather', 'Cycling'),
-('Bob', 'Bloggs', 'Stamp Collecting'),
-('Joe', 'Strummer', 'Music'),
-('Mike', 'Feather', 'Drinking')
