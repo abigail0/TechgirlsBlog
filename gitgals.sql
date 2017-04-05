9d280324ef9e6bf56e9207116df1f7052756b449
MelUnderwood <matint@iorek.plus.com>
2017-04-05 17:32:58 +0100

    Add files via upload

--- gitgals.sql
+++ gitgals.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `gitgals` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `gitgals`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: gitgals
@@ -47,6 +45,39 @@ INSERT INTO `blog` VALUES (1,'2017-04-02 13:05:00','A Boring Title','Hello World
 UNLOCK TABLES;
 
 --
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `CommentID` int(11) NOT NULL AUTO_INCREMENT,
+  `CommentTimeStamp` datetime NOT NULL,
+  `CommentTitle` varchar(255) NOT NULL,
+  `CommentMainEntry` varchar(1028) NOT NULL,
+  `CommentAccepted` varchar(3) NOT NULL DEFAULT 'No',
+  `UserID` int(11) DEFAULT NULL,
+  `BlogID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`CommentID`),
+  KEY `UserID` (`UserID`),
+  KEY `BlogID` (`BlogID`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`BlogID`) REFERENCES `blog` (`BlogID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (1,'2017-04-05 15:55:53','The first Comment','This is the comment from MelUnderwood for the first blog by MUnderwood, not very interesting!','No',2,1);
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -71,6 +102,10 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES (1,'MUnderwood','a94a8fe5ccb19ba61c4c0873d391e987982fbbd3','matint@iorek.plus.com'),(2,'MelUnderwood','test','matint@iorek.plus.com');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'gitgals'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -81,4 +116,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-04-04  7:19:42
+-- Dump completed on 2017-04-05 17:29:25
